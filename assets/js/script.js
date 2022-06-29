92fcfde76d34080f0099319cb48691c1b8aa568b
sallaball <sallaball1206@gmail.com>
2022-06-29 04:38:01 -0500

    updating html and styling

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+// create date
+
+var currentDayEl = document.querySelector("#currentDay");
+
+currentDayEl.textContent = moment().format("dddd, MMMM Do YYYY");
+
+
+//add lists
+
+// var createTask = function(taskHour, taskEvent, taskList) {
+//     var taskUlList = $("<ul>").addclass("container");
+//     var taskLi = $("<li>")
+//     .addclass("hour")
+//     .text(taskHour);
+//     var taskEvent = $("<p>")
+//     .addclass("description")
+//     .text("taskEvent")
+
+//     taskUlList.append(taskLi, taskEvent);
+
+//     $(".container").append(taskList);
+// };
+
+// console.log(createTask);
+
+
+
