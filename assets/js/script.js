195cf898b44da8949f0393ced27d8f3f09d0aaad
sallaball <sallaball1206@gmail.com>
2022-06-30 11:56:47 -0500

    still working on bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
-var time = moment();
+
+var text = {};
 
 // create date
 
@@ -8,67 +9,62 @@ var currentDayEl = document.querySelector("#currentDay");
 currentDayEl.textContent = moment().format("dddd, MMMM Do YYYY");
 
 
-
 //save textarea by clicking "save" button
-
-var saveText = function() {
-$(".saveBtn").on("click", function() {
+$(document).ready(function() {
+  
+    $(".saveBtn").on("click", function() {
     var text = $(this).siblings(".description").val();
     var times = $(this).parent().attr("id");
     localStorage.setItem(text, times);
-    // console.log(times);
-    // console.log(text);
-      
+// console.log(times);
+// console.log(text);
+    });
 
-})};
+    var loadText = function() {
+        tasks = JSON.parse(localStorage.getItem("text"));
+    };
+loadText();
+});
 
-saveText();
 
 
 
-//get saved items from local storage
 
-var textBox = localStorage.getItem(".descritpion");
+
+
+
+
 
 
 
 
 
   //check what time it is
-
+    
   var checkTime = function() {
     var hour = $(".hour").text().trim();
 
     var time = moment().hour();
     console.log(time, hour);
 
-    $(".hour").removeClass(".present .past .future");
+    $(".time-block").removeClass(".present .past .future");
 
     if (moment().isAfter(time)) {
-        $(this).addClass(".past");
+        $(".time-block").addClass(".past");
     } else if (moment().isBefore(time)) {
-        $(this).addClass(".future");
+        $(".time-block").addClass(".future");
     } else {
-        $(this).addClass(".present");
+        $(".time-block").addClass(".present");
     }
+    
   };
-
   checkTime();
+  
 
  
 
 
-// //if hour has passed, add grey background
-var pastHour = function(textEl) {
-    var hour = $(textEl).find(".hour").text().trim();
-    var time = moment(hour, "L").set("hour", 17);
-    $(textEl).removeClass("future");
-    console.log("time");
-  
-    if (moment().isAfter(time)) {
-      $(textEl).addClass("future");
-    } 
-  };
+
 
 
 
