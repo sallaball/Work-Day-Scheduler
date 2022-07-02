3e355f5cc23eeba4497d920699cddf653fd54886
sallaball <sallaball1206@gmail.com>
2022-07-02 11:42:53 -0500

    after a tutoring session, I got a bit further with the work day assignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,66 +8,43 @@ var currentDayEl = document.querySelector("#currentDay");
 
 currentDayEl.textContent = moment().format("dddd, MMMM Do YYYY");
 
+var timesArr = ["8:00", "9:00", "10:00", "11:00", "12:00"]
 
 //save textarea by clicking "save" button
-$(document).ready(function() {
-  
-    $(".saveBtn").on("click", function() {
-    var text = $(this).siblings(".description").val();
-    var times = $(this).parent().attr("id");
-    localStorage.setItem(text, times);
-console.log(times);
-console.log(text);
-    });
+$(document).ready(function () {
+    var time = parseInt(moment().format("HH"));
+   
+    var buildBlocks = function () {
+        for (i=0; i<timesArr.length; i++) {
+            var newRow = $("<div>").attr("id", timesArr[i]).attr("class", "time-block row")
+            var newHour = $("<div>").attr("class", "hour col-md-1").text(timesArr[i]);
+            var newTA = $("<textarea>").val(localStorage.getItem(timesArr[i]))
+            
+            if(i < time - 8) {
+                newTA.attr("class", "description past col-md-10")
+            }else if(i == time - 8) {
+                newTA.attr("class", "description present col-md-10")             
+            }else {
+                newTA.attr("class", "description future col-md-10")
+            }
+            
+            var newBtn = $("<button>").attr("class", "btn saveBtn col-md-1").html("<i class='fas fa-save'></i>");
+            
+            newBtn.on("click", function () {
+                var text = $(this).siblings(".description").val();
+                var times = $(this).parent().attr("id");
+                localStorage.setItem(times, text);
+                console.log(times);
+                console.log(text);
+            });
 
-    var loadText = function() {
-        tasks = JSON.parse(localStorage.getItem("text"));
+            newRow.append(newHour)
+            newRow.append(newTA)
+            newRow.append(newBtn)
+
+            $(".container").append(newRow)
+        }
     };
-loadText();
+
+    buildBlocks();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //check what time it is
-    
-  var checkTime = function() {
-    var hour = $(".hour").text().trim();
-
-    var time = moment().hour();
-    console.log(time, hour);
-
-    $(".time-block").removeClass(".present .past .future");
-
-    if (moment().isAfter(time)) {
-        $(".time-block").addClass(".past");
-    } else if (moment().isBefore(time)) {
-        $(".time-block").addClass(".future");
-    } else {
-        $(".time-block").addClass(".present");
-    }
-    
-  };
-  checkTime();
-  
-
- 
-
-
-
-
-
-
-
-
-
